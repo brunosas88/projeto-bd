fa8c2838984790ace9b656f6d7a732059463142b
brunosas88 <bruno.sas@live.com>
2022-04-01 19:13:05 -0300

    add specification

--- src/main/java/br/com/letscode/moduloix/projetobd/produto/repository/ProdutoRepository.java
+++ src/main/java/br/com/letscode/moduloix/projetobd/produto/repository/ProdutoRepository.java
@@ -2,10 +2,11 @@ package br.com.letscode.moduloix.projetobd.produto.repository;
 
 import br.com.letscode.moduloix.projetobd.produto.model.Produto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
+public interface ProdutoRepository extends JpaRepository<Produto, Integer>, JpaSpecificationExecutor {
 
     Produto findProdutoByCodigo(String codigo);
 
